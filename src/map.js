5dffcc00a563b165a1de50652fd377ffe8c42c89
Sonny Iannone <iannos@uw.edu>
2021-02-26 13:57:42 -0800

    draw map

--- src/map.js
+++ src/map.js
@@ -2,27 +2,22 @@ import mcpp from '../static/Micro_Community_Policing_Plans.geojson'
 "use strict";     // the code should be executed in "strict mode".
                   // With strict mode, you can not, for example, use undeclared variables
 
-d3.json(mcpp).then(function(data) {
-var projection = d3.geoEquirectangular()
-  .scale(200)
-  .translate([200, 150]);
+d3.json(mcpp).then(function(geojson) {
 
-var geoGenerator = d3.geoPath()
-  .projection(projection);
+var projection = d3.geoMercator().fitSize([800, 1600], geojson);
+var path = d3.geoPath().projection(projection);
+var g = d3.select('#content g.map')
+g.selectAll('path')
+  .data(geojson.features)
+  .enter()
+  .append('path')
+  .attr('d', path)
+  .style("fill", "white")
+  .style("stroke-width", "1")
+  .style("stroke", "black");
 
-function update(data) {
-  var u = d3.select('#content g.map')
-    .selectAll('path')
-    .data(data.features);
-
-  u.enter()
-    .append('path')
-    .attr('d', geoGenerator);
-}
-
-update(data);
-console.log(data.features);
 });
+
 
 
 
