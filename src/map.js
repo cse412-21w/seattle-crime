e5b1e8d984ab3f98fdb5c11f35402744a88c56c2
Sonny Iannone <iannos@uw.edu>
2021-02-26 22:04:50 -0800

    update

--- src/map.js
+++ src/map.js
@@ -1,12 +1,14 @@
 import mcpp from '../static/Micro_Community_Policing_Plans.geojson'  
+import points from '../static/SPD_offense_coord_w_year_0.csv'
 "use strict";     // the code should be executed in "strict mode".
                   // With strict mode, you can not, for example, use undeclared variables
 
-d3.json(mcpp).then(function(geojson) {
 
-var projection = d3.geoMercator().fitSize([800, 1600], geojson);
+d3.json(mcpp).then(function(geojson) {
+var projection = d3.geoMercator().fitSize([500, 800], geojson);
 var path = d3.geoPath().projection(projection);
 var g = d3.select('#content g.map')
+
 g.selectAll('path')
   .data(geojson.features)
   .enter()
@@ -15,6 +17,14 @@ g.selectAll('path')
   .style("fill", "white")
   .style("stroke-width", "1")
   .style("stroke", "black");
+ 
+ g.selectAll('circle')
+	.data([[-122.312077769,47.69761617]]).enter()
+	.append("circle")
+	.attr("cx", function (d) { projection(d); return projection(d)[0]; })
+	.attr("cy", function (d) { return projection(d)[1]; })
+	.attr("r", "5px")
+	.attr("fill", "red")
 
 });
 
